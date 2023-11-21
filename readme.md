55d7c581577b2ab9eb3fc08c74dae75329758550
karimdib <karim.dib2001@gmail.com>
2023-11-21 15:23:16 +0100

    ex php intro

---
+++ readme.md
@@ -0,0 +1,12 @@
+# Descrizione:
+
+- Creare un form PHP che permetta di inviare un campo in POST: una parola da censurare.
+- Il form deve inviare i dati ad un altro file php. In questo file php dove avere una variabile che contiene del testo (a vostra scelta)  e dovete recuperare il parametro inviato dal form, e fare le seguenti operazioni:
+  - stampare il testo
+  - stampare la lunghezza del testo originale
+  - stampare di nuovo il paragrafo, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare
+  - stampare la lunghezza del nuovo paragrafo
+# BONUS
+aggiungete nel form una textarea per poter inviare oltre alla parola da censurare anche il testo in cui effettuare le sostituzioni
+
+Buon lavoro  
---
+++ text.php
@@ -0,0 +1,38 @@
+
+
+
+
+<?php
+ $name = 'karim';
+ $nome= $_POST['marco'];
+
+ $text='Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias eaque unde praesentium enim repellendus quia possimus numquam
+ consequuntur officiis quos similique, doloremque sunt aut architecto corporis quibusdam iste quo libero
+ ex voluptas voluptatem ut officia omnis.ex voluptas voluptatem ut officia omnis.ex voluptas voluptatem ut officia omnis.
+ Maxime fugit quo harum laborum, minus laboriosam dolores dolorum repudiandae atque saepe modi assumenda! Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias eaque unde praesentium enim repellendus quia possimus numquam
+ consequuntur officiis quos similique, doloremque sunt aut architecto corporis quibusdam iste quo libero
+ ex voluptas voluptatem ut officia omnis.ex voluptas voluptatem ut officia omnis.ex voluptas voluptatem ut officia omnis.
+ Maxime fugit quo harum laborum, minus laboriosam dolores dolorum repudiandae atque saepe modi assumenda!';
+
+
+ $new_text= str_replace($nome,"*** ", $text);
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Intro php</title>
+</head>
+<body>
+    <p><?php echo $text ?></p>
+    <p>Lunghezza:<?php echo strlen($text) ?> caratteri </p>
+    <p>
+        <?php echo  $new_text?>
+    </p>
+    <p> Lunghezza: <?php echo strlen($new_text) ?> caratteri </p>
+
+</body>
+</html>
